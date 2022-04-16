69e7d4738f688c1bec59c90e705815a3d77e5f95
Bryan-lam <89774942+Bryan-lam@users.noreply.github.com>
2022-04-16 22:13:07 +0800

    Add files via upload

--- JetpackGameAssignment/player.h
+++ JetpackGameAssignment/player.h
@@ -6,25 +6,25 @@
 #define BOOST_KEY 'w'
 
 class Player {
-    public:
-        Player(WINDOW * win, int y, int x, char c);
+public:
+    Player(WINDOW * win, int y, int x, char c);
 
-        void mvup();
-        void mvdown();
-        void mvleft();
-        void mvright();
-        int getInput();
-        void Display();
+    void mvup();
+    void mvdown();
+    void mvleft();
+    void mvright();
+    int getInput();
+    void Display();
 
-    private:    
-        int xPos, yPos;
-        int yMax = 1;
-        int xMax = 1;
-        int yMin, xMin;
-        char icon;
-        WINDOW *curwin;
+private:    
+    int xPos, yPos;
+    int yMax = 1;
+    int xMax = 1;
+    int yMin, xMin;
+    char icon;
+    WINDOW *curwin;
 
-        bool kbhit();
+    bool kbhit();
 };
 
 Player::Player(WINDOW * win, int y, int x, char c) {
@@ -32,8 +32,10 @@ Player::Player(WINDOW * win, int y, int x, char c) {
     yPos = y;
     xPos = x;
     getmaxyx(curwin, yMin, xMin);
-    keypad(curwin, true);
+    // keypad(curwin, true);
     icon = c;
+
+    Display();
 }
 
 int Player::getInput() {
@@ -53,28 +55,23 @@ void Player::mvup() {
     mvwaddch(curwin, yPos, xPos, ' ');
     yPos--;
     if (yPos < yMax) yPos = yMax;
+    Display();
+
+    return;
 }
 
 void Player::mvdown() {
     mvwaddch(curwin, yPos, xPos, ' ');
     yPos++;
     if (yPos > yMin-2) yPos = yMin-2;
-}
+    Display();
 
-void Player::mvleft() {
-    mvwaddch(curwin, yPos, xPos, ' ');
-    xPos--;
-    if (xPos < xMax) xPos = xMax;
-}
-
-void Player::mvright() {
-    mvwaddch(curwin, yPos, xPos, ' ');
-    xPos++;
-    if (xPos > xMin-2) xPos = xMin-2;
+    return;
 }
 
 void Player::Display() {
     mvwaddch(curwin, yPos, xPos, icon);
+    return;
 }
 
 bool Player::kbhit() {
